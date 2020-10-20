3841a3d4f7a51674b651c7ff846be7b87ec8f3e6
David Lopez <70594015+dalopz@users.noreply.github.com>
2020-10-19 20:31:21 -0500

    código corregido y prueba imprimir info banderas

--- Juego.java
+++ Juego.java
@@ -1,11 +1,33 @@
+import java.util.*;
+import java.util.Arrays;
+
+
 public class Juego {
+
+	public static int[] arregloAleatorio(int[] array){
+		Random rgen = new Random();
+
+		for (int i = 0; i<array.length; i++){
+			int randomPosition = rgen.nextInt(array.length);
+			int temp = array[i];
+			array[i] = array[randomPosition];
+			array[randomPosition] = temp;
+		}
+		return array;
+	}
 
 	public static int[] crearIndices(int total)
 	{
-		
+		int indices[] = new int[total];
+		int contador = 0;
+		for(int i=0; i<indices.length; i++){
+			indices[i] = contador;
+			contador+=20;
+		}
+		return indices;
 	}
-	
-	public static void imprimirbandera(string[] banderas, int indice)
+
+	public static void infoBandera(String[] banderas, int indice)
 	{
 		String fila[];
 
@@ -23,25 +45,25 @@ public class Juego {
 
 	public static void capitales()
 	{
-		System.out.println("CAPITALEESSSS");
+	System.out.println("gfgr");
 	}
 
-	public static void personajes()
+	/*public static void personajes()
 	{
 		System.out.println("comprombacion invocacion");
-	}
+	}*/
 
 	public static void menuPrincipal()
 	{
 		int centinela = 0;
-		int indices[] = crearIndices(20);
-		indices = RandomizeArray(indices);
+		int indices[] = crearIndices(24);
+		indices = arregloAleatorio(indices);
 		String banderas[] = ConsoleFile.read("info_banderas.csv");
 
 		do{
 			System.out.println();
 			System.out.println("Escoge una opcion: ");
-			System.out.println("1. Jugar con personajes");
+			System.out.println("1. Informacion bandera");
 			System.out.println("2. Jugar con capitales");
 			System.out.println("3. SALIR");
 			centinela = ConsoleInput.getInt();
@@ -49,7 +71,7 @@ public class Juego {
 			switch(centinela)
 			{
 				case 1: System.out.println();
-						personajes();
+						infoBandera(banderas, indices[0]);
 						break;
 				case 2: System.out.println();
 						capitales();
